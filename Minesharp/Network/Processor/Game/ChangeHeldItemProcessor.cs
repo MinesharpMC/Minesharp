53bac2fd8f9de45aa6dd88ee1dbfb70125dbe19e
Roxeez <roxeeztsu@gmail.com>
2022-08-15 20:13:46 +0200

    Add change held item

---
+++ Minesharp/Network/Processor/Game/ChangeHeldItemProcessor.cs
@@ -0,0 +1,31 @@
+using Minesharp.Common.Enum;
+using Minesharp.Packet.Game.Client;
+using Minesharp.Packet.Game.Server;
+
+namespace Minesharp.Network.Processor.Game;
+
+public class ChangeHeldItemProcessor : PacketProcessor<ChangeHeldItemPacket>
+{
+    protected override void Process(NetworkSession session, ChangeHeldItemPacket packet)
+    {
+        var world = session.Player.World;
+        var players = world.GetPlayers();
+        foreach (var player in players)
+        {
+            var knownEntities = player.KnownEntities;
+            if (knownEntities.Contains(session.Player.Id))
+            {
+                player.SendPacket(new EquipmentPacket
+                {
+                    EntityId = session.Player.Id,
+                    Slot = EquipmentSlot.MainHand,
+                    Item = new ItemInfo
+                    {
+                        Amount = 1,
+                        Material = Material.Stone
+                    }
+                });
+            }
+        }
+    }
+}
